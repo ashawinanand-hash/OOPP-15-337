1e6f40183db5a0ceba082cdf05abc90d30851903
ashawinanand-hash <ashawinanand@gmail.com>
2026-09-18 09:36:22 +0530

    commit

---
+++ Lab_02/exp_07.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int fun1(int &x){
+    x = x + (x/10);
+    return x;
+}
+
+int fun2(int *x){
+    
+}
+
+int main(){
+int salary = 1000;
+int &newsalary = salary;
+fun1(newsalary);
+cout<<salary;
+}
