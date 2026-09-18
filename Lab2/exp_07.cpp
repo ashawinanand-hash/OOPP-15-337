c9f540590e1f5b6e6e93ef0abfcc444cfad1ff88
ashawinanand-hash <ashawinanand@gmail.com>
2026-09-18 10:19:45 +0530

    commit

--- Lab2/exp_07.cpp
+++ Lab2/exp_07.cpp
@@ -7,12 +7,15 @@ int fun1(int &x){
 }
 
 int fun2(int *x){
-    
+    *x = *x + (*x/10);
+    return *x;
 }
 
 int main(){
 int salary = 1000;
-int &newsalary = salary;
-fun1(newsalary);
+// int &newsalary = salary;
+// fun1(newsalary);
+int *newsalary = &salary;
+fun2(newsalary);
 cout<<salary;
 }
