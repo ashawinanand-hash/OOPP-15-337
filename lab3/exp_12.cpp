e7351629a45b1bd2d8109c5d4fdb0b877ba82f9c
ashawinanand-hash <ashawinanand@gmail.com>
2026-09-25 10:14:57 +0530

    commit

---
+++ lab3/exp_12.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+void area(double radius){
+   cout<<"area : "<<2*3.14*radius<<endl;
+}
+void area(double length,double width){
+    cout<<"area : "<<length*width<<endl;
+}
+void area(int side){
+    cout<<"area : "<<side*side<<endl;
+}
+void area(double base,double height,bool traingle){
+    cout<<"area : "<<0.5*(base)*(height)<<endl;
+}
+int main(){
+    area(5.0);
+    area(5,6);
+    area(4);
+    area(4,5,1);
+
+}
